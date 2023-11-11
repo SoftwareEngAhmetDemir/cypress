74863707abc62afe1a27c78ff42d6624c8fc11b0
Ahmet <a.demir@extratik.com>
2023-11-11 17:45:14 +0300

    add new test

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,12 +1,21 @@
 /* ==== Test Created with Cypress Studio ==== */
-it('test form', function() {
+let x = [
+  "This test has been Written By Ahmet Demir to show the cypress abilities ... ... ... !!! !!! !!!",
+];
+it("test form", function () {
   /* ==== Generated with Cypress Studio ==== */
-  cy.visit('localhost:3000');
-  cy.get('[name="name"]').clear('ah');
-  cy.get('[name="name"]').type('ahmet demir');
-  cy.get('[name="surename"]').clear('he');
-  cy.get('[name="surename"]').type('hekko cypress');
+  cy.visit("localhost:3000");
+  cy.get('[name="name"]').clear("ah");
+  cy.get('[name="name"]').type("ahmet demir");
+  cy.get('[name="surename"]').clear("he");
+  cy.get('[name="surename"]').type("hekko cypress");
   cy.get('[type="submit"]').click();
-  cy.get('#typing').type('This test has been Written By Ahmet Demir to show the cypress abilities ... ... ... !!! !!! !!!')
+  let y = cy.get("#typing");
+
+  x.forEach((e) => {
+    setTimeout(() => {
+      y.type(e);
+    }, 500);
+  });
   /* ==== End Cypress Studio ==== */
 });
